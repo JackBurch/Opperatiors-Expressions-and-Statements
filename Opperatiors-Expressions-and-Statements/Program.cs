7e5c9688402e0ddb045ba3c51213e9b23364feb4
JackBurch <31357591+JackBurch@users.noreply.github.com>
2017-09-12 12:04:15 -0600

    Original
    Sorry for posting it late, just forgot to commit to master

--- Opperatiors-Expressions-and-Statements/Program.cs
+++ Opperatiors-Expressions-and-Statements/Program.cs
@@ -10,6 +10,49 @@ namespace Opperatiors_Expressions_and_Statements
     {
         static void Main(string[] args)
         {
-        }
+            //Here I'm declaring variables
+            int x, y, a, b;
+            //Assigning values to these int
+            x = 3;
+            y = 2;
+            a = 1;
+            b = 0;
+
+            //additional math opperators
+
+            //addition 
+            x = 3 + 4;
+            //subtraction 
+            x = 4 - 3;
+            //multiplication
+            x = 10 * 5;
+            //division
+            x = 10 / 5;
+            //evaulation opperators
+            //equalitys 
+            if( x == y)
+            {
+            }
+            //Greater than opperators
+            if(x > y)
+            {
+            }
+            //Less than opperators
+            if(x < y)
+            {
+            }
+        
+            //Conditional Opperators
+            //Conditional AND
+            if (x > y) && (a > b){
+            }
+            //Conditional OR 
+            if (x > y) || (a > b){
+            }
+            // in line conditional operator
+            string message = (x == 1) ? "Car" : "Boat";
+            //Member acess and Method invpocation
+            Console.WriteLine("Hi");
+            }
     }
 }
